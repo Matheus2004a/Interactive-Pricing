b3cbc690ff2ce74b34cfeda649154c0515c3cccd
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-01-23 22:29:44 -0300

    Adicionando valores por ano e com desconto

--- main.js
+++ main.js
@@ -1,51 +1,55 @@
 const priceByMonth = document.querySelector(".price-by-month")
 const quantifyPageViews = document.querySelector(".title-info-card")
+const monthOrYear = document.querySelector(".month-or-year")
 const inputChoosePrice = document.querySelector("#input-progress")
 const togglePrice = document.querySelector(".button-toggle")
 const progressBar = document.querySelector("#progress-bar")
 
 inputChoosePrice.addEventListener("input", validityPrice)
 
-let price = 0
+const listPrices = [8, 12, 16, 24, 36]
+const listQuantifyPageViews = ["10K", "50K", "100K", "500k", "1M"]
 
 function validityPrice() {
-    if (inputChoosePrice.value == 0) {
-        price = 8
-        quantifyPageViews.innerHTML = "10k PAGEVIEWS"
-        priceByMonth.innerHTML = "$8.00"
+    const valueActual = inputChoosePrice.value
+
+    if (valueActual != "") {
+        priceByMonth.innerHTML = `$${listPrices[valueActual].toFixed(2)}`
+        quantifyPageViews.innerHTML = `${listQuantifyPageViews[valueActual]} PAGEVIEWS`
     }
-    else if (inputChoosePrice.value == 1) {
-        price = 12
-        quantifyPageViews.innerHTML = "50k PAGEVIEWS"
-        priceByMonth.innerHTML = "$12.00"
-    }
-    else if (inputChoosePrice.value == 2) {
-        price = 16
-        quantifyPageViews.innerHTML = "100k PAGEVIEWS"
-        priceByMonth.innerHTML = "$16.00"
-    }
-    else if (inputChoosePrice.value == 3) {
-        price = 24
-        quantifyPageViews.innerHTML = "500k PAGEVIEWS"
-        priceByMonth.innerHTML = "$24.00"
-    }
-    else {
-        price = 36
-        quantifyPageViews.innerHTML = "1M PAGEVIEWS"
-        priceByMonth.innerHTML = "$36.00"
-    }
-    const value = inputChoosePrice.value
-    let percent = (value / 4) * 100;
+
+    let percent = (valueActual / 4) * 100;
     inputChoosePrice.style.background = `linear-gradient(to right, var(--strong-cyan-slider-background) ${percent}%, var(--light-grayish-blue-empty-slider-bar) ${percent}%)`
+
+    if (togglePrice.checked === false) {
+        calculatePriceMonth()
+    } else {
+        calculatePriceYear()
+    }
 }
 
-togglePrice.addEventListener("click", calculateDiscount)
+togglePrice.addEventListener("change", calculateDiscount)
 
 function calculateDiscount() {
     validityPrice()
-    if (togglePrice.checked) {
-        let valueDiscount = price * 25 / 100
-        price = price - valueDiscount
-        priceByMonth.innerHTML = `$${price.toFixed(2)}`
+    if (togglePrice.checked === true) {
+        calculatePriceYear()
+    } else {
+        calculatePriceMonth()
     }
+}
+
+function calculatePriceYear() {
+    let price = listPrices[inputChoosePrice.value]
+    let valueYear = price * 12
+    let valueDiscount = valueYear * 0.25
+    price = valueYear - valueDiscount
+    priceByMonth.innerHTML = `$${price.toFixed(2)}`
+    monthOrYear.innerHTML = "/ year"
+}
+
+function calculatePriceMonth() {
+    let price = listPrices[inputChoosePrice.value]
+    priceByMonth.innerHTML = `$${price.toFixed(2)}`
+    monthOrYear.innerHTML = "/ month"
 }
